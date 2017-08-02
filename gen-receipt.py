87383ba9437f773e68d70c2d22b6b722595c2637
moba <mo@localhost>
2017-08-02 19:25:41 +0200

    initial release using python uno

---
+++ gen-receipt.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+import logging
+
+DEFAULT_TEMPLATE="templates/single.odt"
+DEFAULT_OUTPUT="new.pdf"
+LOGLEVEL=logging.INFO
+
+"""gen-receipt.py
+
+Example:
+
+    ./gen-receipt.py 203 "Hans Meier, Apfelstraße 24, 02199 Groden" 24.2.2014
+INFO:root:Validating inputs...
+INFO:root:Trying to connect to Libreoffice...
+INFO:root:Loading templates/single.odt...
+INFO:root:Replacing strings in templates/single.odt
+INFO:root:Writing to new.pdf...
+
+ by Moritz Bartl <moritz@headstrong.de>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import string
+import getopt, sys
+import uno
+
+import click
+import time
+import zipfile
+import tempfile
+import os
+import shutil
+import locale
+
+import zahlwort
+
+from unohelper import Base, systemPathToFileUrl, absolutize
+from os import getcwd
+from os.path import splitext
+from com.sun.star.beans import PropertyValue
+from com.sun.star.uno import Exception as UnoException
+from com.sun.star.io import IOException, XOutputStream
+
+class OutputStream( Base, XOutputStream ):
+    def __init__( self ):
+        self.closed = 0
+    def closeOutput(self):
+        self.closed = 1
+    def writeBytes( self, seq ):
+        sys.stdout.write( seq.value )
+    def flush( self ):
+        pass
+
+def findInDoc(doc, searchstring):
+    searchDescriptor = doc.createSearchDescriptor()
+    searchDescriptor.SearchCaseSensitive = True
+    searchDescriptor.SearchString = searchstring
+    return doc.findFirst(searchDescriptor)
+
+def replaceInDoc(doc, old, new):
+    searchDescriptor = doc.createSearchDescriptor()
+    searchDescriptor.SearchCaseSensitive = True
+    searchDescriptor.SearchString = old
+    found = doc.findFirst(searchDescriptor)
+    if not found:
+        logging.warn('Placeholder ' + old + ' not found in template document!')
+    while found:
+        found.String = new
+        found = doc.findNext(found.End, searchDescriptor)
+
+@click.command()
+@click.argument('amount', type=float)
+@click.argument('address')
+@click.argument('donation_date', default=time.strftime('%d.%m.%Y'))
+@click.option('--template', '-t', type=click.Path(exists=True, file_okay=True, dir_okay=False), default=DEFAULT_TEMPLATE)
+@click.option('--outputfile', '-o', type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=True), default=DEFAULT_OUTPUT)
+def cli(amount, address, donation_date, template, outputfile):
+    retVal = 0
+    # validate optional donation date
+    logging.info('Validating inputs...')
+    try:
+        donation_datetime = time.strptime(donation_date, '%d.%m.%Y')
+    except ValueError:
+        raise ValueError("Incorrect data format, should be DD.MM.YYYY")
+
+    donation_date = time.strftime('%d.%m.%Y', donation_datetime)
+    amount_words = zahlwort.float2text(amount)
+    address_split = address.split(",")
+    address_split = list(map(str.strip, address_split))
+    addressee = '\n'.join(address_split)
+    addressline = ', '.join(address_split)
+
+    try:
+        logging.info("Trying to connect to Libreoffice...")
+        url = "uno:socket,host=localhost,port=2002;urp;StarOffice.ComponentContext"
+        filterName = "writer_pdf_Export"
+        extension  = "pdf"
+
+        ctxLocal = uno.getComponentContext()
+        smgrLocal = ctxLocal.ServiceManager
+
+        resolver = smgrLocal.createInstanceWithContext(
+                 "com.sun.star.bridge.UnoUrlResolver", ctxLocal )
+        ctx = resolver.resolve( url )
+        smgr = ctx.ServiceManager
+
+        desktop = smgr.createInstanceWithContext("com.sun.star.frame.Desktop", ctx )
+
+        cwd = systemPathToFileUrl( getcwd() )
+        outProps = (
+            PropertyValue( "FilterName" , 0, filterName , 0 ),
+            PropertyValue( "SelectPdfVersion", 0, "1", 0 ),
+	        PropertyValue( "Overwrite" , 0, True , 0 ),
+            PropertyValue( "OutputStream", 0, OutputStream(), 0)
+	    )
+
+        inProps = PropertyValue( "Hidden" , 0 , True, 0 ),
+
+        logging.info("Loading " + DEFAULT_TEMPLATE + "...")
+
+        fileUrl = absolutize(cwd, systemPathToFileUrl(DEFAULT_TEMPLATE) )
+        try:
+            doc = desktop.loadComponentFromURL( fileUrl , "_blank", 0, inProps )
+
+            if not doc:
+                raise UnoException( "Couldn't open stream for unknown reason", None )
+
+            logging.info('Replacing strings in ' + DEFAULT_TEMPLATE)
+
+            replaceInDoc(doc, '_ADDRESSEE', addressee)
+
+            replaceInDoc(doc, '_FULLADDRESS', addressline)
+            replaceInDoc(doc, '_AMOUNTNUM', locale.currency(amount))
+            replaceInDoc(doc, '_AMOUNTINWORDS', amount_words)
+            replaceInDoc(doc, '_DONATIONDATE', donation_date)
+
+            destUrl = absolutize( cwd, systemPathToFileUrl(DEFAULT_OUTPUT) )
+            logging.info("Writing to " + DEFAULT_OUTPUT + "...")
+            doc.storeToURL(destUrl, outProps)
+        except IOException as e:
+            sys.stderr.write( "Error during conversion: " + e.Message + "\n" )
+            retVal = 1
+        except UnoException as e:
+            sys.stderr.write( "Error ("+repr(e.__class__)+") during conversion:" + e.Message + "\n" )
+            retVal = 1
+        if doc:
+            doc.dispose()
+
+    except UnoException as e:
+        sys.stderr.write( "Error ("+repr(e.__class__)+") :" + e.Message + "\n" )
+        retVal = 1
+
+    sys.exit(retVal)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=LOGLEVEL)
+    locale.setlocale(locale.LC_MONETARY, ('de','utf-8'))
+    cli()
---
+++ zahlwort.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+"""zahlwort.py outputs a string that describes a float in German words (Euros)
+
+Example:
+
+    zahlwort.py 21921.20
+    > Einundzwanzigtausendneunhunderteinundzwanzig Euro und zwanzig Cent
+
+num2text originally in PHP by Thorsten Rotering <support@rotering-net.de>
+ https://rotering-net.de/tut/php-num2text.html
+
+zahlwort.py ported and slightly extended in Python
+ by Moritz Bartl <moritz@headstrong.de>
+
+ explicitly licensed under MIT with permission of the original author
+ - Thanks Thomas!
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+from math import floor,modf
+import click
+
+NUMERALS=['null', 'ein', 'zwei', 'drei', 'vier', 'fünf', 'sechs', 'sieben', 'acht', 'neun',
+          'zehn', 'elf', 'zwölf', 'drei~', 'vier~',
+          'fünf~', 'sech~', 'sieb~', 'acht~', 'neun~']
+TENNER=['','','zwanzig', 'dreißig', 'vierzig', 'fünfzig', 'sechzig', 'siebzig', 'achtzig', 'neunzig']
+GROUP_SUFFIX=[['',''],['tausend','tausend'], ['e Million ',' Millionen '],['e Milliarde ',' Milliarden ']]
+NUMERAL_SIGN='minus'
+INFIX='und'
+HUNDREDS_SUFFIX='hundert'
+
+def num2text_group(number, group_level=0):
+    if (number==0): return ''
+    group_number = number % 1000;
+    res = ''
+
+    if (group_number == 1):
+        res = NUMERALS[1] + GROUP_SUFFIX[group_level][0]
+    elif (group_number > 1):
+        first_digit = floor(group_number / 100)
+        if (first_digit):
+            res = NUMERALS[first_digit] + HUNDREDS_SUFFIX
+        else:
+            res = ''
+
+        last_digits = group_number % 100
+        second_digit = floor(last_digits / 10)
+        third_digit = last_digits % 10
+
+        if (last_digits == 0):
+            res += ''
+        elif (last_digits == 1):
+            res += NUMERALS[1] + 's'
+        elif (last_digits <= 19):
+            res += NUMERALS[last_digits].replace('~', NUMERALS[10])
+        else:
+            if (third_digit):
+                res += NUMERALS[third_digit] + INFIX
+            res += TENNER[second_digit]
+
+        res += GROUP_SUFFIX[group_level][1]
+
+    number = floor(number / 1000)
+    group_level += 1
+
+    return num2text_group(number, group_level) + res
+
+def num2text(number):
+    prefix=''
+    if (number == 0):
+        return NUMERALS[0] # null
+    if (number < 0):
+        prefix = NUMERAL_SIGN + ' ' # minus …
+    return prefix + num2text_group(abs(number))
+
+    click.echo(zahl)
+
+def float2text(number_float):
+    cent, euro = modf(number_float)
+    cent = round(cent * 100)
+    zahlwort = num2text(int(euro)) + ' Euro'
+    if (cent>0):
+        zahlwort += ' und ' + num2text(int(cent)) + ' Cent'
+    zahlwort = zahlwort[0].upper() + zahlwort[1:]
+    return zahlwort
+
+@click.command()
+@click.argument('zahl', type=float)
+def cli(zahl):
+    click.echo(float2text(zahl))
+
+if __name__ == '__main__':
+        cli()
